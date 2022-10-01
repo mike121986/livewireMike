1605475b407e34b8474db207cb643d0ec73e4adf
mike trujillo <38016591+mike121986@users.noreply.github.com>
2022-10-01 14:08:17 -0500

    ejecutamos los factorie a aprtir del seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Storage;
+use App\Models\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,7 +21,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
+        Storage::deleteDirectory('public/posts');
+        Storage::makeDirectory('public/posts');
+        User::factory()->create();
         \App\Models\Post::factory(100)->create();
     }
 }

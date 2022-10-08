221be4b41e5bcd0ab1a7173b75cc500491be134d
mike trujillo <38016591+mike121986@users.noreply.github.com>
2022-10-08 15:00:53 -0500

    cambios

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Livewire;
 
-use Livewire\Component;
-/* para rescatar todos los elementos de una base de datos primero debemos mandar al modelo */
 use App\Models\Post;
+/* para rescatar todos los elementos de una base de datos primero debemos mandar al modelo */
+use Livewire\Component;
+use Illuminate\Support\Facades\Storage;
+/* clase que nos ayus a subir fotos al servido */
+use Livewire\WithFileUploads;
 
 class ShowPosts extends Component
 {
-    public $search , $post;
+    use WithFileUploads;
+    public $search , $post, $image,$identificador,$existe;
 
     public $sort = 'id' ;
     public $direction = 'desc';
+
+    /* incializamos una propiedad con el valor de false ya que este sera par abrir el modal  */
+    public $open_edit = false;
 
     /* emitimos una escucha por el componente createPost(para que una vez que inserte el dato se actualize la tabla) 
         ponemos un metodo protected con la la palabra reservada "$listeners" delante de este enrte corchetes, el nombre de 
@@ -40,6 +47,19 @@ class ShowPosts extends Component
         //para recuperar del metodo GET
         //$this->name = $name;
     //}
+
+    /* peopiedades para mopstrar los datos en el modal */
+    protected $rules=[
+        'post.title' => 'required',
+        'post.content' => 'required'
+    ];
+
+    public function mount(){
+        
+        /* inicalizamos el identificador con un numero aleatorio */
+        $this->identificador = rand();
+        $this->post = new Post();
+    }
 
     /**
      * esta clase mantiene siempre esta verificando 
@@ -80,5 +100,28 @@ class ShowPosts extends Component
 
     public function edit(Post $post){
         $this->post = $post;
+        $this->existe = Storage::disk('public')->exists(Storage::url($this->post->image));
+        $this->open_edit = true;
+    }
+
+    public function update(){
+         // validamos
+         $this->validate();
+         /* si hay algo almacenado en la variable imagen del objeto post */
+        if($this->image){
+         Storage::delete([$this->post->image]);
+         /* subimos la nueva imagen */
+         $this->post->image = $this->image->store('public/posts');
+        }
+         $this->post->save();
+         // reseteamos el modal
+         $this->reset(['open_edit','image']);
+ 
+         $this->identificador = rand();
+         // emitimos el renderizado o actuazliar datos 
+        /*por este momento no tenemos que renderizar nada ya que como esta dentro de la misma clase no es necesario
+          $this->emitto('show-posts','render'); */
+         // emitimos una alerta
+         $this->emit('alert','El post se actualizo Corerrectamente');
     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -73,20 +73,20 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white">
-                    @foreach ($posts as $post)
+                    @foreach ($posts as $item)
                         <tr>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
                                 <div class="flex items-center">
                                     <div>
-                                        <div class="text-sm leading-5 text-gray-800">{{$post->id}}</div>
+                                        <div class="text-sm leading-5 text-gray-800">{{$item->id}}</div>
                                     </div>
                                 </div>
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                                <div class="text-sm leading-5 text-blue-900">{{$post->title}}</div>
+                                <div class="text-sm leading-5 text-blue-900">{{$item->title}}</div>
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
-                                {{$post->content}}
+                                {{$item->content}}
                             </td>
                             <td
                                 class="px-6 py-4 whitespace-no-wrap  border-b border-gray-500 text-sm leading-5">
@@ -94,7 +94,7 @@
                                     class="py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">View
                                     Details</button> 
                                     @livewire('edit-post', ['post' => $post], key($post->id))--}}
-                                    <a class="btn btn-green" wire:click="edit({{$post}})"><i class="fas fa-edit"></i></a>
+                                    <a class="btn btn-green" wire:click="edit({{$item}})"><i class="fas fa-edit"></i></a>
                                     
                             </td>
                         </tr>
@@ -104,10 +104,55 @@
             </table>
         @else
             <div class="px-6 py-4">
-                No existe Ningun registro
+                No existe Ningun registroMotel Pegas
             </div>
         @endif
         </x-table>
 
     </div>
+
+
+    <x-jet-dialog-modal wire:model="open_edit">
+        <x-slot name="title">
+            Editar el post {{$post->title}}
+        </x-slot>  
+        <x-slot name="content">
+            <div wire:loading wire:tarjet="image" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Cargando imagen</strong>
+                <span class="block sm:inline">Espere hasta que la imagen halla cargando!!...</span>                
+              </div>
+            @if ($image)
+                <img class="mb-4" src="{{$image->temporaryUrl()}}" alt=""> 
+            @else
+           {{--  {{$post->image}} --}}
+           <img src="{{Storage::url($post->image)}}" alt="">   
+              @if (!$existe)
+              <img src="{{$post->image}}" alt="">       
+              @endif
+                        
+                        
+
+            @endif
+            <div class="mb-4">
+                <x-jet-label value="Titulo del post"/>
+                <x-jet-input wire:model="post.title" type="text" class="w-full"/>
+            </div>
+            <div class="mb-4">
+                <x-jet-label value="Contenido del post"/>
+                <textarea wire:model="post.content" rows="6" class="form-control w-full"></textarea>
+            </div>
+            <div class="mb-4">
+                <input type="file" wire:model='image' id="{{$identificador}}">
+                <x-jet-input-error for="image"/>
+            </div>
+        </x-slot>  
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$set('open_edit',false)">
+                Cancelar
+            </x-jet-secondary-button>
+            <x-jet-danger-button wire:click="update" wire:loading.attr="disbled" class="disabled:opacity-25">
+                Actualizar
+            </x-jet-danger-button>
+        </x-slot>  
+    </x-jet-dialog-modal>
 </div>
